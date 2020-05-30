c299af47c8ca976561d1a25aef9b1a301d9f4537
Rik <rikpeeters11@hotmail.com>
2020-05-30 02:13:02 +0200

    Vue Testing

---
+++ ui/tests/unit/breed.module.spec.js
@@ -0,0 +1,21 @@
+import store from "@/store"
+describe("mutations", () => {
+
+})
+
+
+// describe("Create", () => {
+//
+// })
+// describe("Update", () => {
+//
+// })
+// describe("Read", () => {
+//
+// })
+// describe("Delete", () => {
+//
+// })
+// describe("List", () => {
+//
+// })
